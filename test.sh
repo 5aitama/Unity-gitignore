6b6c9a9e1751cf0ada9403c027212415dfe92509
5aitama サイタマ <44070842+5aitama@users.noreply.github.com>
2020-07-22 15:34:41 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,4 +3,4 @@
 fileName=".gitignore"
 
 wget -q --output-document=$fileName https://raw.githubusercontent.com/github/gitignore/master/Unity.gitignore
-echo "\n# For mac user\n**/*.DS_Store" >> $fileName
+printf "\n# For mac user\n**/*.DS_Store" >> $fileName
