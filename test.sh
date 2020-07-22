1c73c5c6f6c38ea45c73fd026a055de9423b592a
5aitama サイタマ <44070842+5aitama@users.noreply.github.com>
2020-07-22 16:03:25 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-fileName=".gitignore"
+filePath=".gitignore"
 gitignoreUrl="https://raw.githubusercontent.com/github/gitignore/master/Unity.gitignore"
 separator="\n"
 
 # File(s) or folder(s) to add in the .gitignore file.
 extra=("# For Mac OS user\n**/.DS_Store")
 
-wget -q --output-document=$fileName $gitignoreUrl
+wget -q --output-document=$filePath $gitignoreUrl
 
 for index in ${!extra[@]}; do
   if [ $index = 0 ]; then
-    printf $separator >> $fileName
+    printf $separator >> $filePath
   fi
   
-  printf "${extra[$index]}$separator" >> $fileName
+  printf "${extra[$index]}$separator" >> $filePath
   
 done
