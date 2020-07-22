bc9c5f02eb94140aa4d1f750a1b9de9cea05a0c4
5aitama サイタマ <44070842+5aitama@users.noreply.github.com>
2020-07-22 21:44:36 +0200

    Update .sh

--- .sh
+++ .sh
@@ -4,5 +4,5 @@ filePath=".gitignore"
 gitignoreUrl="https://raw.githubusercontent.com/github/gitignore/master/Unity.gitignore"
 extraUrl="https://raw.githubusercontent.com/5aitama/Unity-gitignore/master/extras"
 
-curl $gitignoreUrl --output $filePath
+curl -s $gitignoreUrl --output $filePath
 printf "\n$(curl -s $extraUrl)\n" >> $filePath
